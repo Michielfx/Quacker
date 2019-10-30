c0abeca31f0e4cdecbacab4ddf1ef598b73d4519
Noah <37075859+NoahViste@users.noreply.github.com>
2019-10-30 15:06:00 +0100

    Update popularity_account_noah.py

--- eca/popularity_account_noah.py
+++ eca/popularity_account_noah.py
@@ -4,5 +4,8 @@
 def pop_at_acc(ctx, tweet):
     # Noah is working on this
     for mention in (tweet["entities"]["user_mentions"]):
-        print(mention["screen_name"])
+        try:
+            ctx.pop_dic[mention["screen_name"]] += 1
+        except:
+            ctx.pop_dic[mention["screen_name"]] = 1
 
